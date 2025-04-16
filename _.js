ac6533b985846bec4165b36e2c954f89613c9554
Tommaso Berti <tommaso.berti@helptek.it>
2025-04-16 17:54:57 +0200

    add initial implementation of module

--- _.js
+++ _.js
@@ -1,13 +1,15 @@
 const _ = {};
 //to test this instruction, run the command: node _.js
 
+_.clamp = (num, lower, upper) => {
+    num = Math.min(Math.max(num, lower), upper)
+    return num
+}
+//to test this instruction, run the command: node clamp.js
 
+_.inRange = (num, start, end) => {
 
-
-
-
-
-
+}
 
 
 
