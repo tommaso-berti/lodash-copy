4ec3e3f2d1a696601756e74f9860862158aa0c66
Tommaso Berti <tommaso.coding@gmail.com>
2025-04-17 08:58:21 +0200

    Revert "update test instructions and improve inRange function"
    This reverts commit 975d709b6e364e8c43762d03349e4639747943ba.

--- _.js
+++ _.js
@@ -7,18 +7,11 @@ _.clamp = (num, lower, upper) => {
 }
 //to test this instruction, run the command: node test/clamp.js
 
-_.inRange = (num, start, end) => {
-    if(!end) {
-        end = start;
-        start = 0;
-    }
-    if (start > end) [start, end] = [end, start]
+_.inRange = (num, start = 0, end) => {
     return (num >= start) && (num < end)
 }
 
-_.words = (str) => {}
-
-console.log(_.inRange(5, 10, 6)) // true
+console.log(_.inRange(5, 10)) // true
 
 
 
